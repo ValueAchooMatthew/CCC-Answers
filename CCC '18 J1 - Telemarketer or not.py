efaac6690d6ecd59c8fb702ba6f7d8e61e518151
ValueAchooMatthew <78985742+ValueAchooMatthew@users.noreply.github.com>
2022-02-23 21:04:14 -0500

    Delete CCC '18 J1 - Telemarketer or not.py

--- CCC '18 J1 - Telemarketer or not.py
+++
@@ -1,9 +0,0 @@
-id = []
-
-id.append(int(input())), id.append(int(input())), id.append(int(input())), id.append(int(input()))
-
-if (id[0] == 8 or id[0] == 9) and (id[3] == 8 or id[3] == 9) and (id[1] == id[2]):
-    print("ignore")
-
-else:
-    print("answer")
